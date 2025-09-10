398a86688ed222454e2b49ad060ca60a36ffc5d8
Bhargav <2210030264@klh.edu.in>
2025-09-10 11:57:16 +0530

    BHARGAV

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            BHARGAV'S URL SHORTENER
           </Title>
 
           <Text
